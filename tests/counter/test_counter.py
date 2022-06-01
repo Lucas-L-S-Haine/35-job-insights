8f2f0e3251c7e98bf71291b004b052c575d1f4fb
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-06-01 17:44:56 -0300

    feat: implementa test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.counter import count_ocurrences
+import pytest
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("tests/counter/languages.txt", "python") == 2
+    assert count_ocurrences("tests/counter/languages.txt", "javascript") == 2
+    assert count_ocurrences("tests/counter/languages.txt", "rust") == 1
+    try:
+        count_ocurrences("tests/counter/languages.txt", 5)
+    except AttributeError:
+        pytest.mark.xfail(reason="Invalid input type")
